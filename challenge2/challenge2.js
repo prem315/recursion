5bcb4d94120b57fbefd1079857eba1640d004db3
prem315 <prem.patel3105@gmail.com>
2021-06-08 21:58:50 +0530

    fibonacci series

--- challenge2/challenge2.js
+++ challenge2/challenge2.js
@@ -8,7 +8,7 @@ function sum(num) {
 }
 
 let res = sum(5)
-console.log(res)
+// console.log(res)
 
 // recursion
 // 5 + sum(5-1)
@@ -17,3 +17,19 @@ console.log(res)
 // 5 + 4 + 3 + 2 + sum(2-1)
 // 5 + 4 + 3 + 2 + 1 -> break base condition
 // 15
+
+function fib(num) {
+    let num1 =  0
+    let num2 = 1
+    let nextNum = num1 + num2
+    console.log(num1)
+    console.log(num2)
+    while(nextNum <= num) {
+        nextNum = num1 + num2;
+        num1 = num2;
+        num2 = nextNum
+
+        console.log(nextNum)
+    }
+}
+
---
+++ challenge3/challenge3.js
@@ -0,0 +1,12 @@
+// program to display fibonacci sequence using recursion
+function fibonacci(num) {
+    if(num < 2) {
+        return num;
+    }
+    else {
+        return fibonacci(num-1) + fibonacci(num - 2);
+    }
+}
+
+fibonacci(10)
+
