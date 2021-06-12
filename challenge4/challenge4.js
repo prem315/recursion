97f8ea37eb2aa3d40b14b32d232cf139125ddf19
prem315 <prem.patel3105@gmail.com>
2021-06-12 13:38:25 +0530

    reverse string recursively

---
+++ challenge4/challenge4.js
@@ -0,0 +1,16 @@
+// reverse the string recursively 
+
+function reverseStr(str) {
+    console.log(str);
+    if(str === "") {
+        return str
+    } else {
+        let newStr = str[str.length - 1]
+        return newStr + reverseStr(str.substr(0, str.length - 1))
+    }
+}
+
+const reverseString = reverseStr("prem")
+console.log(reverseString)
+
+
