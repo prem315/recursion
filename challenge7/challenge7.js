37a41eb7a542872af38376adadf0f29550f26322
prem315 <prem.patel3105@gmail.com>
2021-06-14 22:20:59 +0530

    first occurrence in an arr using recurtion

--- challenge7/challenge7.js
+++ challenge7/challenge7.js
@@ -1,14 +1,14 @@
 // first occurance of a number in an array
 
-function firstOccuranceOfaNumber(arr, num, index) {
+function firstoccurrenceOfaNumber(arr, num, index) {
     if(arr.length === index) {
         return -1
     }
     if(arr[index] === num) {
         return index
     }
-    return firstOccuranceOfaNumber(arr, num, index + 1)
+    return firstoccurrenceOfaNumber(arr, num, index + 1)
 }
 
-let res = firstOccuranceOfaNumber([5 , 4, 1, 3, 2, 3, 1], 1, 0)
+let res = firstoccurrenceOfaNumber([5 , 4, 1, 3, 2, 3, 1], 1, 0)
 console.log(res);
